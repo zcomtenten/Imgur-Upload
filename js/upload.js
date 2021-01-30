152e54440f6b07497e0243cebea6ce3e2a24f4f3
Hoang Duy <duyplusdz@gmail.com>
2021-01-30 16:55:13 +0700

    Update file

--- js/upload.js
+++ js/upload.js
@@ -19,8 +19,9 @@ var feedback = function(res) {
         var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
         document.querySelector('.status').classList.add('bg-success');
         var content =
-            'Image : ' + '<br><input class="image-url" value=\"' + get_link + '\"/>' 
-             + '<img class="img" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
+            'Image:' + '<br><input class="image-url" value=\"' + get_link + '\"/>'
+                     + '<br><input class="image-url" value=\"<img src="' + get_link + '"/>\"/>'
+                     + '<img class="img" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
         addImg('.status', content);
     }
 };
